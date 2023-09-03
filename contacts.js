43e834f8298a6f8eb16f53b8d310c628f6dcbf29
IhorHodebskyi <121040851+IhorHodebskyi@users.noreply.github.com>
2023-09-03 12:04:55 +0300

    add

--- contacts.js
+++ contacts.js
@@ -1,28 +1,60 @@
-// contacts.js
-const fs = require("fs/promises");
 const fs = require("fs").promises;
 const path = require("path");
+const { nanoid } = require("nanoid");
 
-const contactsPath = require("./db/contacts.json");
+const contactsPath = path.join(__dirname, "./db/contacts.json");
 
-// TODO: задокументувати кожну функцію
-function listContacts() {
-  // ...твій код. Повертає масив контактів.
-  fs.readFile(filename, [options]);
-}
+const listContacts = async () => {
+  const data = await fs.readFile(contactsPath);
+  return JSON.parse(data);
+};
 
-function getContactById(contactId) {
-  // ...твій код. Повертає об'єкт контакту з таким id. Повертає null, якщо контакт з таким id не знайдений.
-}
+const getContactById = async (contactId) => {
+  // for yargs
+  //   const id = String(contactId);
+  const list = await listContacts();
+  const result = list.find((item) => item.id === contactId);
+  return result || null;
+};
 
-function removeContact(contactId) {
-  // ...твій код. Повертає об'єкт видаленого контакту. Повертає null, якщо контакт з таким id не знайдений.
-}
+const removeContact = async (contactId) => {
+  // for yargs
+  //   const id = String(contactId);
+  const list = await listContacts();
+  const index = list.findIndex((item) => item.id === contactId);
+  if (index === -1) {
+    return null;
+  }
+  const [result] = list.splice(index, 1);
+  await fs.writeFile(contactsPath, JSON.stringify(list, null, 2));
+  return result;
+};
 
-function addContact(name, email, phone) {
-  // ...твій код. Повертає об'єкт доданого контакту.
-}
+const addContact = async (name, email, phone) => {
+  const list = await listContacts();
+  const newContact = { id: nanoid(), name, email, phone };
+  list.push(newContact);
+  await fs.writeFile(contactsPath, JSON.stringify(list, null, 2));
+  return newContact;
+};
+
+const updateContact = async (contactId, name, email, phone) => {
+  // for yargs
+  //   const id = String(contactId);
+  const list = await listContacts();
+  const index = list.findIndex((item) => item.id === contactId);
+  if (index === -1) {
+    return null;
+  }
+  list[index] = { contactId, name, email, phone };
+  await fs.writeFile(contactsPath, JSON.stringify(list, null, 2));
+  return list[index];
+};
 
 module.exports = {
   listContacts,
+  getContactById,
+  addContact,
+  removeContact,
+  updateContact,
 };
